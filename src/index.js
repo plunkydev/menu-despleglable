1a100f1b6e05b42693dc41c5d677e561825334bd
plunkydev <plunkydesign@gmail.com>
2024-12-15 18:52:31 -0500

    Agregado evento para mostrar el menu al pasar el mouse.. sigue en proseso

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-const hello = document.getElementById('container')
+import './style.css'
+import imgMenuHamburgesa from './images/menuIcon.png'
+const container = document.getElementById('container')
+let nav = `<nav class="navContainer">
+            <div class="logo">Logo</div>
+            <div class="btnContainer" id="btnContiner">
+                <button id="btn" class="btn"><img id="imgMenuHamburgesa" class="imgMenuHamburgesa" src="${imgMenuHamburgesa}" alt="menu hamburgesa"></button>
+                <div class="menuList" id="menuList">
+                    <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                </div>
+            </div>
+    </nav>`
+let page = `<div class="page">
+                <h1>Page</h1>
+            </div>`
 
-hello.innerHTML = `
-    <h1>Menu Desplegable</h1>`
+container.innerHTML = nav + page
+
+const btn = document.getElementById('btn')
+btn.addEventListener('mouseover', () => {
+  const btnContainer = document.getElementById('btnContiner')
+  const menuList = document.getElementById('menuList')
+  btnContainer.style.display = 'grid'
+  menuList.style.display = 'flex'
+})
