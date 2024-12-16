7fdc3ea77733866818d52f183851f8d1b88c4def
plunkydev <plunkydesign@gmail.com>
2024-12-16 17:02:58 -0500

    Refactor: Implementación de un sistema dinámico de navegación y menú interactivo con estilos e imágenes importadas. Se reemplazó el uso directo de estilo inline y elementos estáticos por generación dinámica de DOM, modularidad CSS e imágenes desde assets locales. Optimización de eventos para mostrar/ocultar menú.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,53 @@
 import './style.css'
 import imgMenuHamburgesa from './images/menuIcon.png'
+import close from './images/close.png'
 const container = document.getElementById('container')
-let nav = `<nav class="navContainer">
-            <div class="logo">Logo</div>
-            <div class="btnContainer" id="btnContiner">
-                <button id="btn" class="btn"><img id="imgMenuHamburgesa" class="imgMenuHamburgesa" src="${imgMenuHamburgesa}" alt="menu hamburgesa"></button>
-                <div class="menuList" id="menuList">
-                    <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
+
+const nav = document.createElement('nav')
+nav.innerHTML = `<nav class="navContainer">
+                <div class="logo">Plunky Dev</div>
+                <div class="btnContainer" id="btnContiner">
+                    <button id="btn" class="btn"><img id="imgMenuHamburgesa" class="imgMenuHamburgesa" src="${imgMenuHamburgesa}" alt="menu hamburgesa"></button>
+                    <div class="menuList" id="menuList">
+                        <div class="menuItem">
+                            <button id="btnHome">Home</button>
+                            <button id="btnAbout">About</button>
+                            <button id="btnContact">Contact</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-    </nav>`
-let page = `<div class="page">
-                <h1>Page</h1>
-            </div>`
+        </nav>`
 
-container.innerHTML = nav + page
+const page = document.createElement('main')
+page.innerHTML = `<div class="page">
+                    <h1>Home</h1>
+                </div>`
 
+const footer = document.createElement('footer')
+footer.classList.add('footer')
+footer.innerHTML = `<p>Footer</p>`
+
+container.append(nav, page, footer)
 const btn = document.getElementById('btn')
-btn.addEventListener('mouseover', () => {
-  const btnContainer = document.getElementById('btnContiner')
+const img = document.getElementById('imgMenuHamburgesa')
+function closeMenu () {
   const menuList = document.getElementById('menuList')
-  btnContainer.style.display = 'grid'
+  menuList.style.display = 'none'
+  img.src = imgMenuHamburgesa
+  img.style.width = '30px'
+  img.style.height = '30px'
+  btn.removeEventListener('click', closeMenu)
+  btn.addEventListener('click', showMenu)
+}
+
+function showMenu () {
+  const menuList = document.getElementById('menuList')
   menuList.style.display = 'flex'
-})
+  img.src = close
+  img.style.width = '20px'
+  img.style.height = '20px'
+  btn.removeEventListener('click', showMenu)
+  btn.addEventListener('click', closeMenu)
+}
+btn.addEventListener('mouseover', showMenu)
+btn.addEventListener('click', closeMenu)
