11ac79294fbf0d801780cb4bd9ff9fac66a40b7f
plunkydev <plunkydesign@gmail.com>
2024-12-20 15:07:24 -0500

    Agregado modulo para crear las paginas que se renderizaran en el main

---
+++ src/pages.js
@@ -0,0 +1,13 @@
+function home (params) {
+  return `<h1>Home</h1>`
+}
+
+function about (params) {
+  return `<h1>About</h1>`
+}
+
+function contact (params) {
+  return `<h1>Contact</h1>`
+}
+
+export { home, about, contact }
